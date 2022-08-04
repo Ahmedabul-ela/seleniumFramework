e0cac8819c96f52800c46c85ea0c54b1e31decfd
Ahmed <ahmad_mahmoud28@yahoo.com>
2022-08-04 20:30:00 +0200

    Chrome Headless

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -72,6 +72,10 @@ public class TestBase extends AbstractTestNGCucumberTests{
 			String[] phantomJsArgs = ("--web-security=no","--Ignore-SSl-errors=yes");
 			caps.setCapability(PhantomJSDriverService.PHANTOM_CLI_ARGS,phantomJsArgs);
 			driver = new PhatomJsDriver(caps);
+		}else if (browsername.equalsIgnoreCase("chrome-headless"){
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			options.addArguments("--window-size=1920,1080");
 		}
 		driver.manage().window().maximize();
 		driver.navigate().to("https://demo.nopcommerce.com/");
