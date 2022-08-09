bb7a68a49c93d8974b8322aee5fefa9f016df27d
Ahmed <ahmad_mahmoud28@yahoo.com>
2022-08-09 15:41:13 +0200

    Saucelab Configuration

--- src/test/java/tests/TestBase2.java
+++ src/test/java/tests/TestBase2.java
@@ -12,12 +12,18 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import data.LoadProperities;
 import utilities.helper;
 
 public class TestBase2 {
 
 	public static String BaseURL = "https://demo.nopcommerce.com/";
 	protected ThreadLocal<RemoteWebDriver> driver = null; // save memory location
+
+	//saucelabs configuration
+	public static final String username = LoadProperities.saucelabsuserdata.getProperty("username");
+	public static final String accesskey = LoadProperities.saucelabsuserdata.getProperty("accesskey");
+	public static final String sauceURL = "https://"+username+":"+accesskey+"@"+LoadProperities.saucelabsuserdata.getProperty("seleniumurl");
 
 	@BeforeClass
 	@Parameters({"Browser"})
@@ -26,7 +32,12 @@ public class TestBase2 {
 		driver = new ThreadLocal<>();
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("BrowserName", Browser);
-		driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps));
+		
+		//selenium grid local
+		//driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps));
+		
+		//saucelabs on cloud
+		driver.set(new RemoteWebDriver(new URL(sauceURL),caps));
 		getDriver().navigate().to(BaseURL);
 	}
 	public RemoteWebDriver getDriver() {
